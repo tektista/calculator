b835eaf4d9c9776fa4769ca64be9cbe3ff4b7b93
John Bautista <jnl.bautista.cs@gmail.com>
2022-06-07 21:49:14 +0100

    add complete calculator, add the ability to not divide by 0

--- script.js
+++ script.js
@@ -15,16 +15,88 @@ function divide(x, y) {
 }
 
 function operate(x, y, operator) {
-  return operator(x, y);
+  return window[operator](x, y);
 }
 
-function addToDisplay(innerText) {
-  let text = innerText;
+function addToDisplay(text) {
+  if (text != "=" && text != "Clear" && text != "Delete") {
+    let displayDiv = document.querySelector(".display");
+    displayDiv.innerText = displayDiv.innerText + text;
+  }
+}
+
+function addToResult(solution) {
   let resultDiv = document.querySelector(".result");
-  resultDiv.innerText = resultDiv.innerText + text;
+  resultDiv.innerText = solution;
 }
 
-// ADD event click event lister for number buttons
+function calculate(valueOfButton) {
+  if (valueOfButton == "clear") {
+    let displayDiv = document.querySelector(".display");
+
+    let resultDiv = document.querySelector(".result");
+    resultDiv.innerText = "";
+
+    displayDiv.innerText = "";
+    currentString = "";
+    firstNo = "";
+    secondNo = "";
+    solution = "";
+    operatorVariable = "";
+    return 0;
+  }
+
+  if (valueOfButton.match(/^[0-9]+$/) != null) {
+    currentString = currentString + valueOfButton.toString();
+  }
+
+  if (
+    (valueOfButton == "add" ||
+      valueOfButton == "subtract" ||
+      valueOfButton == "multiply" ||
+      valueOfButton == "divide" ||
+      valueOfButton == "=") &&
+    firstNo == ""
+  ) {
+    firstNo = currentString;
+    currentString = "";
+    operatorVariable = valueOfButton;
+  }
+
+  if (
+    ((valueOfButton == "add" ||
+      valueOfButton == "subtract" ||
+      valueOfButton == "multiply" ||
+      valueOfButton == "divide") &&
+      firstNo != "") ||
+    valueOfButton == "="
+  ) {
+    secondNo = currentString;
+  }
+
+  console.log(firstNo);
+  console.log(secondNo);
+
+  if (firstNo != "" && secondNo != "") {
+    solution = operate(Number(firstNo), Number(secondNo), operatorVariable);
+    addToResult(solution);
+
+    currentString = "";
+    firstNo = solution;
+    secondNo = "";
+    operatorVariable = valueOfButton;
+  }
+}
+
+// GLOBALS
+let currentString = "";
+let firstNo = "";
+let secondNo = "";
+let solution = "";
+
+let operatorVariable = "";
+
+// EVENT LISTENERS
 
 const buttonsContainer = document.querySelectorAll(
   ".buttons-container>*, .clear-delete-container>*"
@@ -35,5 +107,6 @@ console.log(buttonsContainer);
 buttonsContainer.forEach((element) => {
   element.addEventListener("click", function () {
     addToDisplay(element.innerText);
+    calculate(element.value);
   });
 });
